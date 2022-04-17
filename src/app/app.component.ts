f4af2f1cf4844d43ecd1552c95bc83146c795b0d
UBF21 <felipe_2_0_0_2@hotmail.com>
2022-04-17 01:42:20 -0500

    DadosAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadosIzquierda = "../assets/img/dice1.png";
+  dadosDerecha = "../assets/img/dice4.png";
+
+  numero1: number = 2;
+  numero2: number = 1;
+  
+  tirarDatos(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadosIzquierda = "../assets/img/dice" + this.numero1 + ".png";
+    this.dadosDerecha = "../assets/img/dice" + this.numero2 + ".png";
+
+
+  }
 }
